bab290c2fd12ab52b758ed48dd4a683d3e923748
Ali <alphazentral@gmail.com>
2018-09-21 12:55:18 +0800

    Started on Gradle

---
+++ Assignment/core/src/main/java/QuizApplication.java
@@ -0,0 +1,10 @@
+package edu.curtin.quizapplication;
+
+import java.io.*;
+import java.util.*;
+
+public class QuizApplication{
+	public static void main(String[] args){
+		System.out.println("The whole squad on that real shit");
+	}
+}
